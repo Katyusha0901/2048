b3a9108c21a2f5847f70fdd19a2c73e12299b49c
Ekaterina Bondareva <1978231@bk.ru>
2023-06-19 14:23:26 +0700

    gn

--- gametwenty.js
+++ gametwenty.js
@@ -12,6 +12,12 @@ let gameBoard = [
   ["2", "*", "*"],
 ];
 
+function getUserInput(symbol) {
+    const command = prompt(
+      ` Поле выглядит так\n${gameBoard[0]}\n${gameBoard[1]}\n${gameBoard[2]}\n${gameBoard[3]}\nКуда поставить ${symbol}. Напишите 2 координаты: по y и по x, через запятую`
+    );
+}
+
 function randomInteger(min, max) {
   const rand = min - 0.5 + Math.random() * (max - min + 1);
   return Math.round(rand);
@@ -23,8 +29,8 @@ function createPoint() {
     const y = randomInteger(0, 3);
     const x = randomInteger(0, 3);
 
-    if (field[y][x] === "*") {
-      field[y][x] = newCount;
+    if (gameBoard[y][x] === "*") {
+        gameBoard[y][x] = newCount;
       return;
     }
   }
@@ -40,4 +46,5 @@ function generateCount() {
   
    // return emptyCoordinates[randomCoordinateIndex];
   //}
-  
+
+

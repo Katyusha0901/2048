1592ac45606b112503b6939ae03bde62b933c8f6
Ekaterina Bondareva <1978231@bk.ru>
2023-06-28 23:41:16 +0700

    ne poluchaetsya

--- script.js
+++ script.js
@@ -7,32 +7,37 @@ let field = [
 
 while (true) {
   generateNewValue();
-  const turnsValue = requestDirection();
+  let turnsValue = requestDirection();
 
   rotateMultipleTimes(turnsValue);
   shiftAndMerge();
 
-  const revolutionsValue = field.length - turnsValue;
+  let revolutionsValue = field.length - turnsValue;
   rotateMultipleTimes(revolutionsValue);
 }
 
 function generateNewValue() {
+  console.log("функция generateNewValue");
   const y = randomInteger(0, 3);
   const x = randomInteger(0, 3);
   if (field[y][x] === "*") {
     const newValue = Math.random() > 0.75 ? 4 : 2;
     field[y][x] = newValue;
   } else {
-    generateNewValue();
+    return generateNewValue();
   }
 }
 
 function randomInteger(min, max) {
+  console.log("функция randomInteger");
+
   const rand = min - 0.5 + Math.random() * (max - min + 1);
   return Math.round(rand);
 }
 
 function requestDirection() {
+  console.log("функция requestDirection");
+
   const command = prompt(
     ` Поле выглядит так\n${field[0]}\n${field[1]}\n${field[2]}\n${field[3]}\nНапишите направление движения поля`
   );
@@ -60,12 +65,19 @@ function requestDirection() {
 //=============================================================================================================================================================
 
 function rotateMultipleTimes(turnsValue) {
+  console.log("функция rotateMultipleTimes");
+
+  console.log(`${field}`);
+  console.log(`${field[0]}`);
+
   for (let i = 0; i < turnsValue; i++) {
-    rotateField();
+    return rotateField();
   }
 }
 
 function rotateField() {
+  console.log("функция rotateMultipleTimes");
+
   let rotatedField = [];
   for (let i = 0; i < field.length; i++) {
     rotatedField.push([]);
@@ -74,9 +86,13 @@ function rotateField() {
   for (let i = 0; i < field.length; i++) {
     for (let j = 0; j < field.length; j++) {
       rotatedField[j][field.length - 1 - i] = field[i][j];
+
+      //return rotatedField
     }
   }
   field = rotatedField;
+
+  //field = rotatedField
 }
 
 //=======================================================================================================================================
@@ -84,16 +100,19 @@ function rotateField() {
 //=======================================================================================================================================
 
 function shiftAndMerge() {
-  const newField = field.map((str) => {
+  console.log("функция shiftAndMerge");
+
+  field.map((str) => {
     shiftElement(str);
     mergeElements(str);
     const shiftAndMergeElement = shiftElement(str);
     return shiftAndMergeElement;
   });
-  field = newField;
 }
 
 function shiftElement(stringFromField) {
+  console.log("функция shiftElement");
+
   for (let i = 0; i < stringFromField.length; i++) {
     for (let i = 0; i < stringFromField.length; i++) {
       if (stringFromField[i + 1] === "*") {
@@ -105,14 +124,16 @@ function shiftElement(stringFromField) {
 }
 
 function mergeElements(arrayWithShiftedDigits) {
-  const arr = arrayWithShiftedDigits;
-  for (let i = 0; i < arr.length; i++) {
+  console.log("функция mergeElements");
+
+  const array = arrayWithShiftedDigits;
+  for (let i = 0; i < array.length; i++) {
     if (
-      arr[arr.length - i] === arr[arr.length - i - 1] &&
-      arr[arr.length - i] !== "*"
+      array[array.length - i] === array[array.length - i - 1] &&
+      array[array.length - i] !== "*"
     ) {
-      arr[arr.length - i] = arr[arr.length - i - 1] + arr[arr.length - i - 1];
-      arr[arr.length - i - 1] = "*";
+      array[array.length - i] = array[array.length - i - 1] + array[array.length - i - 1];
+      array[array.length - i - 1] = "*";
     }
   }
 }

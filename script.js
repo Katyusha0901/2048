88176f3fa136261eca898e07951acb743eb3367f
Ekaterina Bondareva <1978231@bk.ru>
2023-06-29 00:39:58 +0700

    corr

--- script.js
+++ script.js
@@ -17,7 +17,6 @@ while (true) {
 }
 
 function generateNewValue() {
-  console.log("функция generateNewValue");
   const y = randomInteger(0, 3);
   const x = randomInteger(0, 3);
   if (field[y][x] === "*") {
@@ -29,15 +28,11 @@ function generateNewValue() {
 }
 
 function randomInteger(min, max) {
-  console.log("функция randomInteger");
-
   const rand = min - 0.5 + Math.random() * (max - min + 1);
   return Math.round(rand);
 }
 
 function requestDirection() {
-  console.log("функция requestDirection");
-
   const command = prompt(
     ` Поле выглядит так\n${field[0]}\n${field[1]}\n${field[2]}\n${field[3]}\nНапишите направление движения поля`
   );
@@ -65,19 +60,12 @@ function requestDirection() {
 //=============================================================================================================================================================
 
 function rotateMultipleTimes(turnsValue) {
-  console.log("функция rotateMultipleTimes");
-  console.log(turnsValue);
-  console.log(`${field[0]}\n${field[1]}\n${field[2]}\n${field[3]}`);
-  console.log(`${field[0]}`);
-
   for (let i = 0; i < turnsValue; i++) {
     rotateField();
   }
 }
 
 function rotateField() {
-  console.log("функция rotateMultipleTimes");
-
   let rotatedField = [];
   for (let i = 0; i < field.length; i++) {
     rotatedField.push([]);
@@ -86,13 +74,9 @@ function rotateField() {
   for (let i = 0; i < field.length; i++) {
     for (let j = 0; j < field.length; j++) {
       rotatedField[j][field.length - 1 - i] = field[i][j];
-
-      //return rotatedField
     }
   }
   field = rotatedField;
-
-  //field = rotatedField
 }
 
 //=======================================================================================================================================
@@ -100,8 +84,6 @@ function rotateField() {
 //=======================================================================================================================================
 
 function shiftAndMerge() {
-  console.log("функция shiftAndMerge");
-
   field.map((str) => {
     shiftElement(str);
     mergeElements(str);
@@ -111,8 +93,6 @@ function shiftAndMerge() {
 }
 
 function shiftElement(stringFromField) {
-  console.log("функция shiftElement");
-
   for (let i = 0; i < stringFromField.length; i++) {
     for (let i = 0; i < stringFromField.length; i++) {
       if (stringFromField[i + 1] === "*") {
@@ -124,8 +104,6 @@ function shiftElement(stringFromField) {
 }
 
 function mergeElements(arrayWithShiftedDigits) {
-  console.log("функция mergeElements");
-
   const array = arrayWithShiftedDigits;
   for (let i = 0; i < array.length; i++) {
     if (
